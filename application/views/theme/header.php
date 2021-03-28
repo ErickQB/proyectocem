458665ebff7a1689580ac2248605595f620fcdde
ErickQB <ej27qb@gmail.com>
2021-03-28 00:27:01 -0600

    Cambio Nombre Logo

--- application/views/theme/header.php
+++ application/views/theme/header.php
@@ -15,7 +15,7 @@
       <span class="icon-bar"></span>
       <span class="icon-bar"></span>
     </button>
-    <a class="navbar-brand" href="#">Logotipo</a>
+    <a class="navbar-brand" href="#">CEM</a>
   </div>
 
   <!-- Agrupar los enlaces de navegación, los formularios y cualquier
