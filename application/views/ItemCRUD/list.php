d873a842d7ea0a90c18c4acc8754e8851d1df763
ErickQB <ej27qb@gmail.com>
2021-03-29 19:10:54 -0600

    Agregue una nueva columna

--- application/views/ItemCRUD/list.php
+++ application/views/ItemCRUD/list.php
@@ -16,6 +16,7 @@
           <th>ID</th>
           <th>Nombre</th>
           <th>Descripcion</th>
+          <th>Grado</th>
           <th width="220px">Opciones</th>
       </tr>
   </thead>
@@ -25,6 +26,7 @@
           <td><?php echo $item->ID_Materia; ?></td>
           <td><?php echo $item->Nombre; ?></td>
           <td><?php echo $item->Descripcion; ?></td>
+          <td><?php echo $item->ID_Grado; ?></td>
       <td>
         <a class="btn btn-info" href="<?php echo base_url('itemCRUD/'.$item->ID_Materia) ?>"> Mostrar</a></br>
         <a class="btn btn-primary" href="<?php echo base_url('itemCRUD/edit/'.$item->ID_Materia) ?>"> Editar</a></br>
