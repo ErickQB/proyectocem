937823c8f00bc4d47398e3affa814c62e1a497f6
David Vásquez <dasesavasa13@gmail.com>
2021-03-31 14:59:55 -0600

    Creacion de MateriaGrado.

---
+++ application/views/MateriaGrado/show.php
@@ -0,0 +1,43 @@
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2> Vista Previa Materia</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="<?php echo base_url('MateriaGrado');?>"> Regresar</a>
+        </div>
+    </div>
+</div>
+
+
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>ID</strong>
+            <?php echo $item->ID_Materia; ?>
+        </div>
+    </div>
+
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Nombre:</strong>
+            <?php echo $item->Nombre; ?>
+        </div>
+    </div>
+
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Descripcion:</strong>
+            <?php echo $item->Descripcion; ?>
+        </div>
+    </div>
+
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>ID_Grado:</strong>
+            <?php echo $item->ID_Grado; ?>
+        </div>
+    </div>
+        </div>
+    </div>
+</div>
